529dc4d03c55c5bedf18c2bfb22e19972cc6db3e
Ahmed-Abdella <abdellaa921@gmail.com>
2022-07-18 01:39:48 +0200

    final touches

--- src/pages/dashboard/ProjectsFilter.js
+++ src/pages/dashboard/ProjectsFilter.js
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import dropDownIcon from "../../assets/drop.svg";
+
 const filterList = [
   "all",
   "mine",
@@ -7,26 +10,74 @@ const filterList = [
   "sales",
 ];
 export default function ProjectsFilter({ currentFilter, changeFilter }) {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [dropOpen, setDropOpen] = useState(false);
+  useEffect(() => {
+    window.addEventListener("resize", () => setWidth(window.innerWidth));
+    return window.removeEventListener("resize", () =>
+      setWidth(window.innerWidth)
+    );
+  }, []);
+
+  const breakpoint = 700;
+  console.log(dropOpen);
+
   const handleFilter = (newFilter) => {
     changeFilter(newFilter);
   };
 
   return (
-    <div className="project-filter">
-      <nav>
-        <p>Filter by:</p>
-        {filterList.map((i) => (
-          <button
-            key={i}
-            onClick={() => {
-              handleFilter(i);
-            }}
-            className={currentFilter === i ? "active" : ""}
-          >
-            {i}
-          </button>
-        ))}
-      </nav>
-    </div>
+    <>
+      {width >= breakpoint ? (
+        <div className="project-filter">
+          <nav>
+            <p>Filter by:</p>
+            {filterList.map((i) => (
+              <button
+                key={i}
+                onClick={() => {
+                  handleFilter(i);
+                }}
+                className={currentFilter === i ? "active" : ""}
+              >
+                {i}
+              </button>
+            ))}
+          </nav>
+        </div>
+      ) : (
+        <div className="menu-container">
+          <div className="drop-down-container">
+            <p>
+              filter by: <span>{` ${currentFilter}`}</span>
+            </p>
+            <img
+              src={dropDownIcon}
+              alt="dropDown"
+              onClick={() => {
+                setDropOpen(!dropOpen);
+              }}
+            />
+          </div>
+
+          {dropOpen && (
+            <nav>
+              {filterList.map((i) => (
+                <button
+                  key={i}
+                  onClick={() => {
+                    handleFilter(i);
+                    setDropOpen(false);
+                  }}
+                  className={currentFilter === i ? "active" : ""}
+                >
+                  {i}
+                </button>
+              ))}
+            </nav>
+          )}
+        </div>
+      )}
+    </>
   );
 }
